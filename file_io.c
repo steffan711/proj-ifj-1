483a23737e20f4982b62f5f6d40dfd03b6fec8af
olii <oliver.nemcek@gmail.com>
2013-10-11 21:56:47 +0200

    visual fix, tabulatory nahradene medzerami

--- file_io.c
+++ file_io.c
@@ -6,16 +6,16 @@
 E_ERROR_TYPE check_file_header(char *source_file)
 {
     char pattern[] = "<?php";
-	for(unsigned int i = 0; i<sizeof(pattern)-1; i++) // -1, lebo pole je dlhsie o null terminator
-	{
-	    if(getc(source_file) != pattern[i])
-		{
-		    return E_OTHER;
-		}
-	}
-	if(!isspace(getc(source_file)))
-	{
-	    return E_OTHER;
-	}
-	return E_OK;
+    for(unsigned int i = 0; i<sizeof(pattern)-1; i++) // -1, lebo pole je dlhsie o null terminator
+    {
+        if(getc(source_file) != pattern[i])
+        {
+            return E_OTHER;
+        }
+    }
+    if(!isspace(getc(source_file)))
+    {
+        return E_OTHER;
+    }
+    return E_OK;
 } /** OTESTOVANE **/
