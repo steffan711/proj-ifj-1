2d6938cbd1d444de2a93453f37a901033d8cb1cd
bananik-the-captain <v.cillo18@gmail.com>
2013-10-23 01:08:41 +0200

    Lexikalna analyza
    rozpracovany scanner

---
+++ scanner.c
@@ -0,0 +1,218 @@
+/*
+ * Projekt: IFJ13
+ * Riesitelia: xcillo00, xmarti62, xnemce03, 	xilavs01
+ *
+ * Subor: scanner.c - modul lexikalniho analyzatoru
+ * Autor: Vladimír Čillo, xcillo00
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef enum {
+	E_OPEQ,			// =
+	E_OPLESS,		// <
+	E_OPGREATER,	// >
+	E_OPLESSEQ,		// <=
+	E_OPGREATEREQ,	// >=
+	E_OPPLUS,		// +
+	E_OPMINUS,		// -
+	E_OPMUL,		// *
+	E_OPDIV,		// /
+	E_SEMICL,		// ;
+	E_COMA,			// ,
+	E_OPCONCAT,		// .
+	E_IDENT,		// identifikator
+	E_VAR,			// premenna
+	E_INT,			// integer
+	E_DOUBLE,		// double
+	E_STRING,		// literal
+	E_Lparentheses,	// (
+	E_Rparentheses,	// )
+	E_LRBRACK,		// [
+	E_RRBRACK,		// ]
+	E_LABRACK,		// {
+	E_RABRACK,		// }
+	E_INVALID,		// je to invalid
+	// ** TODO ** Klujcove slova co ? Nezasluzia si vlastnu tabulku ?
+	// E_ELSE,
+	// E_FUNC,
+	// E_IF,
+	// E_RET,
+	// E_WHILE,
+	// E_FALSE,
+	// E_NULL,
+	// E_TRUE,
+	// E_EOF
+} TOKEN_TYPE;
+
+typedef struct token {
+	TOKEN_TYPE ttype;
+	unsigned line;
+	unsigned column;
+	void *data;
+} S_token;
+
+typedef struct _tStringBuffer	// nekonecny retazec [ak nepretecie velkost ... ]
+        {
+		  unsigned part_size;	// velkost
+          unsigned allocated_size; 
+          unsigned size; // aktualny pocet znakov v retaci
+          char *ptr;	// ukazatel na retazec
+        } tStringBuffer;
+
+
+
+/**
+ * @function GimmeString
+ * 
+ * @param velkost v B, po kolkych sa bude retazec zvacsovat
+ * @return stringBuffer 
+ */		
+tStringBuffer* gimme_string(int size) // vytvori abstrakciu nekonecneho retazca
+{
+	tStringBuffer* tmp = malloc(sizeof(tStringBuffer));
+	tmp->part_size = size;
+	tmp->allocated_size = 0;
+	tmp->size = 0;
+	tmp->ptr = NULL;
+	return tmp;
+}
+
+/**
+ * @function stringAppend
+ * 
+ * @param1 ukazatel na nekonecny retazec
+ * @param2 znak ktory treba doplnit
+ * @return E_ERROR_TYPE
+ */	
+
+int append_string(tStringBuffer* string, char znak) // doplni znak na koniec retazca
+{
+	if(string == NULL)
+	{   // TODO : funkcia ktora osetri chybu
+		return E_OTHER;
+	}
+	else if (string-> allocated_size > string-> size ) // este sa donho zmesti
+	{
+		string->ptr[string->size++] = znak;
+	}
+	else // uz sa nezmesti do koze
+	{
+		string->allocated_size += string->part_size; // zvacsime mu velkost o jednu cast
+		string->ptr = realloc(string->ptr,(string->allocated_size)*sizeof(char));
+		if(string->ptr == NULL)
+		{
+			// TODO : funkcia ktora osetri chybu
+			return E_OTHER;
+		}
+		string->ptr[string->size++] = znak;
+	}
+	return E_OK;
+}
+
+int free_string(tStringBuffer* string)
+{
+	if(string->ptr == NULL)
+	{
+		return E_OTHER;
+	}
+	free(string->ptr);
+	free(string);
+	return E_OK;
+}
+
+
+ /** Globalne premenne **/
+ char* current_pos; // aktualna pozicia scannera v subore, prosim neprepisovat
+
+ 
+ 
+typedef enum {
+	INIT,	
+	FINISH,		// EOF
+	t_id,		// identifikator
+	t_var,		// premenna
+	t_int,		// integer
+	t_ass,		// = (assignment)
+	t_inv,		// nieco strasne
+} FSM_STATE; 
+ 
+S_token scanner(char *data)
+{
+
+	current_pos = data;
+	FSM_STATE next_state = INIT;
+	int znak;
+	
+	S_token next_token;
+	next_token.ttype = t_inv;
+	next_token.line = 0;
+	next_token.column = 0;
+	next_token.data = NULL;
+	
+	while ( next_state != FINISH )
+	{	
+		switch(next_state)
+		{	
+			case INIT:		{
+								znak=getc(current_pos);
+								if(isalpha(znak) || znak == '_') // A-Za-z_
+									next_state = t_id;
+								else if(isdigit(znak))
+								{
+									next_state = t_int;
+								}
+								else if (znak < ' ')	// znaky s ASCII hodnotou mensiou ako 32 nie su  sucastou jazyka
+								{
+									next_state = t_inv;
+								}
+								else
+								{
+									switch(znak)
+									{	
+										case '$':	{	next_state = t_var;
+														break;
+													}			
+										case 0:		{
+														next_state = FINISH;
+													}
+													
+										default: 	{	
+														next_token.ttype = E_INVALID;
+														next_token.data = NULL;
+														next_state = t_inv;
+														break;
+													}
+									}
+								}
+							}
+							
+			case t_var:		{	
+								next_state = t_id;
+								return next_token;
+							}			
+			case t_inv:		{
+								return next_token;
+							}
+			case t_id:		{
+								while(isalnum(getc(current_pos)))
+								{
+									
+								}
+								return next_token;
+							}
+			case t_int:		{
+								break;
+							}
+			case t_ass:		{
+								break;
+							}
+						
+			case FINISH:	break;
+		}
+	}
+	return next_token;
+}
+
