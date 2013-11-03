9aad262f6cb2724718e6c3be7ba9fd9bcf1ffc74
bananik-the-captain <v.cillo18@gmail.com>
2013-11-03 18:21:12 +0100

    Critical error fix

--- file_io.c
+++ file_io.c
@@ -81,6 +81,7 @@ E_ERROR_TYPE mmap_file(const char *filename, char **file_pointer)
     if ( check_file_header( source_file ) != E_OK ) // kontrola '<?php' na zaciatku suboru
     {
         fprintf( stderr, "Invalid source file. Exiting ...\n" );
+        free(source_file); // LOL
 		source_file = NULL;
         return E_OTHER;
     }
