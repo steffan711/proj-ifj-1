b5e38425fa015662178ef03a1bacbb0d7183fbeb
olii <oliver.nemcek@gmail.com>
2013-11-26 00:52:31 +0100

    hotfix odstranenej instrukcie

--- debug.c
+++ debug.c
@@ -50,7 +50,7 @@ const char const *TOKEN_NAME[] =
 
 const char const *OPCODE_NAME[] = 
 {
-"CLEAR", "DUMMY", "START", "CREATE", "CALL", "CALL_BUILTIN", "MOV", "RET", "PUSH", "COND", "JMP", "MOVRET", "CONCAT", "EQUAL", "NONEQUAL", "PLUS","MINUS", "DIV", "LESS", "GREATER", "LESSEQ", "GREATEREQ" 
+"DUMMY", "START", "CREATE", "CALL", "CALL_BUILTIN", "MOV", "RET", "PUSH", "COND", "JMP", "MOVRET", "CONCAT", "EQUAL", "NONEQUAL", "PLUS","MINUS", "DIV", "LESS", "GREATER", "LESSEQ", "GREATEREQ" 
 };
 
 /**
