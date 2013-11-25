32c8762dc7dd04969bc36f00795dcd2deb8888f8
olii <oliver.nemcek@gmail.com>
2013-11-26 00:28:45 +0100

    makro podmieneneho vypisu

--- debug.h
+++ debug.h
@@ -14,5 +14,23 @@ extern const char const *OPCODE_NAME[];
 void print_token( T_token* token );
 
 
+/* DEBUG */
+
+#ifdef DEBUG 
+    #define PRINTD(...) \
+        do { \
+            fprintf ( stderr, __VA_ARGS__ ); \
+        } \
+        while(0)
+#else
+    #define PRINTD(...) \
+        do { \
+        ; \
+        } \
+        while(0)
+#endif
+
+/* END DEBUG */
+
 
 #endif
