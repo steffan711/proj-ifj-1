256264c024d92c9c8596fcd85a5c747d625db3fc
olii <oliver.nemcek@gmail.com>
2013-11-28 16:33:40 +0100

    pridany druhy testovaci subor

---
+++ in3.php
@@ -0,0 +1,19 @@
+<?php
+
+$x = put_string("Zadejte cislo pro vypocet faktorialu\n");
+$a = get_string();
+$a = intval($a);
+if ($a < 0)
+{
+    $x = put_string("Faktorial nelze spocitat\n");
+}
+else
+{
+    $vysl = 1;
+    while ($a > 0)
+    {
+        $vysl = $vysl * $a;
+        $a = $a - 1;
+    }
+    $x = put_string("Vysledek je: " .$vysl . "\n");
+}
