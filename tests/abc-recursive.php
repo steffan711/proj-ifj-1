e607aea1b5f0aa9d93ddbd63bc0eaae8be14b6dc
stevo <steffan711@gmail.com>
2013-11-29 02:30:45 +0100

    little changes in test file

--- tests/abc-recursive.php
+++ tests/abc-recursive.php
@@ -1,12 +1,12 @@
 <?php
 function a($param1)
 {
-    if ($param1 > 10)
+    if ($param1 > 5)
     {
         $p = put_string("funkcia a() vola funkciu b()\n");
         $p = b($param1);
     }
-    elseif ($param1 < 10)
+    elseif ($param1 <= 5)
     {
         $p = put_string("funkcia a() vola funkciu c()\n");
         $p = c($param1);
@@ -15,12 +15,12 @@ function a($param1)
 
 function b($param1)
 {
-    if ($param1 > 10)
+    if ($param1 > 5)
     {
         $p = put_string("funkcia b() vola funkciu c()\n");
         $p = c($param1);
     }
-    elseif ($param1 < 10)
+    elseif ($param1 <= 5)
     {
         $p = put_string("funkcia a() vola funkciu c()\n");
         $p = a($param1);
@@ -29,15 +29,15 @@ function b($param1)
 
 function c($param1)
 {
-    if ($param1 > 10)
+    if ($param1 > 5)
     {
         $p = put_string("funkcia c() vola funkciu a()\n");
         $p = a($param1 - 1);
     }
-    elseif ($param1 < 10)
+    else
     {
         return 0;
     }
 }
 
-$p = a(5);
+$p = a(6);
